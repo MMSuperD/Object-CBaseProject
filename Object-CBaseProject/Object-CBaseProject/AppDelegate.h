7a7afeaeb7133405b977cbe6795c2d3976b80526
王丹 <1653591454@qq.com>
2019-03-22 08:53:31 +0800

    2.新建项目

---
+++ Object-CBaseProject/Object-CBaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Object-CBaseProject
+//
+//  Created by sh-lx on 2019/3/22.
+//  Copyright © 2019年 WangDan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Object-CBaseProject/Object-CBaseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Object-CBaseProject
+//
+//  Created by sh-lx on 2019/3/22.
+//  Copyright © 2019年 WangDan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
