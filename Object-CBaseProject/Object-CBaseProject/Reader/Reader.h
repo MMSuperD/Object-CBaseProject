a718b562eecbf608c2fc7159ceae0c828ece765a
Fan <shdjxywd@sina.com>
2019-06-21 11:29:08 +0800

    常用第三方

---
+++ Object-CBaseProject/Object-CBaseProject/Reader/Reader.h
@@ -0,0 +1,17 @@
+//
+//  Reader.h
+//  Object-CBaseProject
+//
+//  Created by 王丹 on 2019/6/21.
+//  Copyright © 2019 WangDan. All rights reserved.
+//
+
+#ifndef Reader_h
+#define Reader_h
+
+//网易新闻那种滑动:
+//1.JXCategoryView  https://github.com/pujiaxin33/JXCategoryView
+
+
+
+#endif /* Reader_h */
